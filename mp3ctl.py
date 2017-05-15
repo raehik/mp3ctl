db629d89bb8e3f4a5cec0ca5bd95aeca98422cf2
Ben Orchard <thefirstmuffinman@gmail.com>
2017-05-15 05:56:23 +0100

    mp3ctl: 4/4 tasks (+ all cmd)

--- mp3ctl.py
+++ mp3ctl.py
@@ -3,21 +3,23 @@
 # Manage a media device (MP3 player).
 #
 
-import sys
-import os
-import argparse
-import subprocess
+import sys, os, argparse, subprocess, logging
 import shutil
 import re
 import fileinput
 import tempfile
-import logging
+import time
 
 class MP3Ctl:
     PL_REFMT_EXT = "m3u8" # fixes Unicode playlists in Rockbox
     PL_REFMT_PREFIX = "/<microSD1>/music" # easy method for making MPD playlists
                                           # work with Rockbox
+    SCROB_LOG = ".scrobbler.log"
+    SCROB_LOG_ARCHIVE_FILE = "{}-scrobbler-log.txt".format(time.strftime("%F-%T"))
+
     ERR_DEVICE = 3
+    ERR_ARGS = 4
+    ERR_SCROBBLER = 5
     ERR_INTERNAL = 10
 
     def __init__(self):
@@ -29,6 +31,7 @@ class MP3Ctl:
             "music":     os.path.join(os.environ["HOME"], "media", "music"),
             "playlists": os.path.join(os.environ["HOME"], "media", "music-etc", "playlists"),
             "lyrics":    os.path.join(os.environ["HOME"], "media", "music-etc", "lyrics"),
+            "scrobbles": os.path.join(os.environ["HOME"], "media", "music-etc", "mp3-scrobbles"),
         }
 
         self.root_tmpdir = tempfile.mkdtemp(prefix="tmp-{}-".format(os.path.basename(__file__)))
@@ -44,6 +47,7 @@ class MP3Ctl:
         self.parser = argparse.ArgumentParser(description="Manage and maintain a music library.")
         self.parser.add_argument("-v", "--verbose", help="be verbose", action="count", default=0)
         self.parser.add_argument("-q", "--quiet", help="be quiet (overrides -v)", action="count", default=0)
+        self.parser.add_argument("-e", "--edit", help="edit scrobble logs before submitting", action="store_true")
         self.parser.add_argument("command", help="command to run")
         self.parser.add_argument("arguments", nargs="*", help="arguments for command")
 
@@ -58,10 +62,12 @@ class MP3Ctl:
         # dictionary of command -> function
         # command aliases are easily specified by adding to the key tuple
         self.cmds = {
+            ("all",):
+                self.cmd_all,
             ("process-scrobbles", "scrobbles"):
-                lambda: self.__show_cmd_help(self.args.arguments),
+                lambda: self.process_scrobbles(self.args.arguments),
             ("cp-music", "music"):
-                lambda: self.__show_cmd_help(self.args.arguments),
+                self.cp_music,
             ("cp-playlists", "playlists"):
                 self.cp_playlists,
             ("cp-lyrics", "lyrics"):
@@ -117,6 +123,7 @@ class MP3Ctl:
         """Try to unmount a media device."""
         self.__ensure_is_device(dev)
         mnt_dir = self.device_dir[dev]
+        self.logger.info("trying to unmount {}...".format(mnt_dir))
         if self.get_shell(["umount", mnt_dir]) == 0:
             self.logger.info("unmounted successfully")
         else:
@@ -174,6 +181,53 @@ class MP3Ctl:
 
         self.__cp_contents(tmpdir, os.path.join(self.device_dir["media"], "lyrics"))
         self.unmount_dev("media")
+
+    def cp_music(self):
+        self.mount_dev("media")
+        self.__cp_contents(self.media_loc["music"], os.path.join(self.device_dir["media"], "music"))
+        self.unmount_dev("media")
+
+    def process_scrobbles(self, args):
+        log_list = []
+        if not args:
+            log_archive_file = os.path.join(self.media_loc["scrobbles"], MP3Ctl.SCROB_LOG_ARCHIVE_FILE)
+            self.mount_dev("sys")
+            try:
+                # archive log
+                shutil.move(os.path.join(self.device_dir["sys"], MP3Ctl.SCROB_LOG),
+                            log_archive_file)
+            except FileNotFoundError:
+                self.logger.info("no scrobbler log present")
+                self.unmount_dev("sys")
+                return
+            self.unmount_dev("sys")
+
+            # remove exec. bit
+            self.get_shell(["chmod", "-x", log_archive_file])
+
+            self.logger.info("log moved from device -> {}".format(log_archive_file))
+            log_list.append(log_archive_file)
+        else:
+            for f in args:
+                if not os.path.isfile(f):
+                    self.exit("not a file: {}".format(f), MP3Ctl.ERR_ARGS)
+                log_list.append(f)
+
+        for log in log_list:
+            self.logger.info("scrobbling {}".format(log))
+            if self.args.edit:
+                self.logger.info("editing {}...".format(log))
+                self.get_shell([os.getenv("EDITOR", "vim"), log])
+            shutil.copy(log, os.path.join(self.root_tmpdir, MP3Ctl.SCROB_LOG))
+            ret = self.get_shell(["qtscrob-cli", "--file", "--location", self.root_tmpdir])
+            if ret != 0:
+                self.exit("qtscrob-cli failed, exiting", MP3Ctl.ERR_SCROBBLER)
+
+    def cmd_all(self):
+        self.process_scrobbles(self.args.arguments)
+        self.cp_playlists()
+        self.cp_lyrics()
+        self.cp_music()
 
 
 if __name__ == "__main__":
